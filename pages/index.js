f4f944afb6b796f7b2cbfeb6f0d77cecc1bd27cd
zyzhang <withzhaoyu0410@gmail.com>
2023-05-24 22:08:32 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import AvatarWrapper from "./components/AvatarWrapper";
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 
 export default function Home() {
   const jiaran = async (id) => {
